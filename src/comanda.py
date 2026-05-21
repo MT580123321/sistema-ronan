25866fe5795ec2264de32648507438555d88f407
MT580123321 <michelgt716@gmail.com>
2026-05-21 12:06:15 -0300

    Update comanda.py

--- src/comanda.py
+++ src/comanda.py
@@ -5,3 +5,11 @@ class Comanda:
 
     def adicionar_item(self, pedido):
         self.itens.append(pedido)
+
+    def calcular_total(self):
+        total = 0
+
+        for item in self.itens:
+            total += item.subtotal()
+
+        return total
