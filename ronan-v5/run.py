72feb4d3be0669090b10a81768b6bfcd6c21f7b2
Andre Rocha <andrefigueredo2706@gmail.com>
2026-06-15 13:54:10 -0300

    Update run.py

--- ronan-v5/run.py
+++ ronan-v5/run.py
@@ -1,7 +1,7 @@
 from app import app
 
 if __name__ == '__main__':
-    print("\n🔥 Churrascaria do Ronan — Sistema Web")
+    print("\n Churrascaria do Ronan — Sistema Web")
     print("   Acesse: http://localhost:5000")
     print("   Login: admin / 123\n")
     app.run(debug=True, port=5000)
